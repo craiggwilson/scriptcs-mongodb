d4f253fff1a8d2a65f204b65a5a72ce31dd19daa
Craig Wilson <craiggwilson@gmail.com>
2013-12-06 20:04:23 -0600

    added in initial project.

---
+++ src/ScriptCs.MongoDB/MongoDBScriptPack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptCs.Contracts;
+
+namespace ScriptCs.MongoDB
+{
+    public class MongoDBScriptPack : IScriptPack
+    {
+        public void Initialize(IScriptPackSession session)
+        {
+            session.AddReference("MongoDB.Bson.dll");
+            session.AddReference("MongoDB.Driver.dll");
+
+            session.ImportNamespace("MongoDB.Bson");
+            session.ImportNamespace("MongoDB.Driver");
+            session.ImportNamespace("MongoDB.Driver.Builders");
+        }
+
+        public IScriptPackContext GetContext()
+        {
+            return new MongoDB();
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
